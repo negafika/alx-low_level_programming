deedb993d4091fb1ad29c8e8f137bb474f89e6a1
negafika17 <negafika17@gmail.com>
2022-07-02 08:36:44 -0700

    why

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  main - This function prints whether n is positive or not
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 + 48; x <= 9 + 48; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
