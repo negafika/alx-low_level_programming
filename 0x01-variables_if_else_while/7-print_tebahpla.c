fcba9ec8fa0ed66753cd07ca2d6f1b5de7507aab
negafika17 <negafika17@gmail.com>
2022-07-02 08:40:36 -0700

    small adjustments

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  main - This function prints whether n is positive or not
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x <= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
