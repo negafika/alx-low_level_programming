e5f963baa53f6c835b46ec5e41420e17781d05b3
negafika17 <negafika17@gmail.com>
2022-07-02 07:56:42 -0700

    message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  main - This function prints whether n is positive or not
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	return (0);
+}
