0e639812f86ebd8b49dde27eacc3b6b61fef0360
negafika <negafika17@gmail.com>
2022-07-05 18:18:55 +0300

    starting out

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        int b = a[i];
+
+        _putchar(b);
+    }
+    _putchar('\n');
+    return (0);
+}
