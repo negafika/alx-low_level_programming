a651057bae61cd5ae451ab2d4261fa719b8fa287
negafika <negafika17@gmail.com>
2022-07-05 18:51:24 +0300

    write

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
  */
 void print_alphabet(void)
 {
-    int i;
+int i;
 
-    for (i = 97; i < 123; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
