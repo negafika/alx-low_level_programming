b0826456f84bac2577189eb271f0ade68cbb1154
negafika <negafika17@gmail.com>
2022-07-05 18:48:22 +0300

    write

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+    int i;
+
+    for (i = 97; i < 123; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+}
