346140a756b7499691f52df4233572b7fa710ba3
negafika <negafika17@gmail.com>
2022-07-05 18:59:37 +0300

    fourth

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code
+ *
+ * Return: Always 0.
+ * @c: character to test.
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+return (0);
+}
