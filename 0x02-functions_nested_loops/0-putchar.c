72bbf7d422bf7f53233112961f6b9f815c82d179
negafika <negafika17@gmail.com>
2022-07-05 18:39:08 +0300

    what about now?

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 int a[8] = {95, 112, 117, 116, 99, 104, 97, 114};
-    int i;
+int i;
 
-    for (i = 0; i < 8; i++)
-    {
-        int b = a[i];
+for (i = 0; i < 8; i++)
+{
+	int b = a[i];
 
-        _putchar(b);
-    }
-    _putchar('\n');
-    return (0);
+	_putchar(b);
+}
+	_putchar('\n');
+	return (0);
 }
