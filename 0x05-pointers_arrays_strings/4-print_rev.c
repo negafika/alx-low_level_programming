7c9f6c2eee6e59fd7878d1a7ae24b919a23e1f5b
negafika <negafika17@gmail.com>
2022-07-11 14:16:23 +0300

    combined

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints in reverse order
+ * @s: input string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int len;
+
+len = 0;
+while (*(s + len) != '\0')
+	len++;
+while (len--)
+	_putchar(*(s + len));
+}
