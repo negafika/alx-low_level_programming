633b6c9c24f8cfbbde7df98cb5256f23729d6a2a
negafika <negafika17@gmail.com>
2022-07-11 20:39:14 +0300

    message

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,10 +16,13 @@ len = 0;
 while (*(str + len) != '\0')
 	len++;
 i = 0;
-while (i < (len / 2))
+while (i > (len / 2) && i < len)
 {
-_putchar(*(str + len - 1 - i));
+if (len % 2 == 0)	
+	_putchar(*(str + i));
+else
+	_putchar(*(str + i + 1));
 i++;
 }
-
+_putchar('\n');
 }
