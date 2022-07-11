32e739582b4f1372e919dc79537a05fa9f762f05
negafika <negafika17@gmail.com>
2022-07-11 20:11:47 +0300

    message

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints the string
+ * @str: is the string to be printed
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+int len;
+
+len = 0;
+while (*(str + len) != '\0')
+	len++;
+i = 0;
+while (i < len)
+{
+_putchar(*(str + i));
+i += 2;
+}
+_putchar('\n');
+}
