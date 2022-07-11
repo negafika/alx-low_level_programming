bd33ef6d7f87b75acf197f965fca88fe433e8f78
negafika <negafika17@gmail.com>
2022-07-11 12:13:30 +0300

    Confusing pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - check out the code
+ *@s: a string to determine it's length
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+int len;
+
+len = sizeof(s);
+return (len);
+}
