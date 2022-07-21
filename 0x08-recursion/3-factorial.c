ac790528e22a6045d9ec7e433b448f6488f5c54e
negafika <negafika17@gmail.com>
2022-07-20 23:49:42 -0700

    message

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of a number
+ * @n: an integer input
+ *
+ * Return: int
+ */
+int factorial(int n)
+{
+if (n < 0)
+	return (-1);
+if(n == 0)
+	return (1);
+return (n * factorial(n - 1);
+
+}
