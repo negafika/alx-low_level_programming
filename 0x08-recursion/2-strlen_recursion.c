e1d934522c9fea9c60461be1b69c0d7a75e2ac3c
negafika <negafika17@gmail.com>
2022-07-20 22:48:27 -0700

    morning

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: the input string
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 1;
+	if (*s)
+	{
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
