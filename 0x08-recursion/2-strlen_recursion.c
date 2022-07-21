b02e8c2efe49e08237f7ae3465ccd833afcd8763
negafika <negafika17@gmail.com>
2022-07-20 23:06:40 -0700

    those

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,13 +11,10 @@ int _strlen_recursion(char *s)
 	int len;
 
 	len = 1;
-	if (*s == '')
-	{
-	return (0);
-	}
-	else if (*(s + 1))
+	if (*s)
 	{
 		len += _strlen_recursion(s + 1);
 	}
-	return (len);
+	else
+		return (len);
 }
